4969aa0f3e091050d8c4a2b2b53094d07f7514fa
bbchu22 <bbchu22@digiwin.com>
2019-01-22 18:15:16 +0800

    first commit

---
+++ src/app/child/child.component.ts
@@ -0,0 +1,31 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-child',
+  templateUrl: './child.component.html',
+  styleUrls: ['./child.component.css']
+})
+export class ChildComponent implements OnInit {
+  show: boolean = true;
+  classFlag: boolean = true;
+  names = ["Aarav", "Martín", "Shannon", "Ariana", "Kai"];
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+  changeClass(): void {
+    this.classFlag = !this.classFlag;
+  }
+
+  getClass(){
+    if(this.classFlag){
+      return 'second';
+    }
+    else{
+      return null;
+    }
+    // 這裡可以用程式動態決定要回傳字串、陣列或物件
+  }
+
+}
